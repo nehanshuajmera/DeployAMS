2de2d46343457b267c8c563e05bedeb6bfc0d1b9
Aman <amansahu5051@gmail.com>
2023-11-01 15:51:55 +0530

    login page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
+import Login from "./pages/Login"
+
 function App() {
 
   return (
     <div className="w-screen overflow-hidden h-screen flex items-center justify-center">
-      <div className=" bg-green-500 h-[170px] w-[170px] rounded-full p-[2px] flex items-center justify-center cursor-pointer hover:scale-110 duration-500">
+      {/* <div className=" bg-green-500 h-[170px] w-[170px] rounded-full p-[2px] flex items-center justify-center cursor-pointer hover:scale-110 duration-500">
         <div className="bg-white w-full h-full flex items-center justify-center rounded-full">
           <h1 className="text-3xl text-green-500  font-bold ">
             Client
           </h1>
         </div>
-      </div>
-
+      </div> */}
+      <Login/>
+      
     </div>
 
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#FE0000",
+        secondary:"#00040f",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        fadedBeige:"#FCF5ED"
+      }
+    },
   },
   plugins: [],
 }
