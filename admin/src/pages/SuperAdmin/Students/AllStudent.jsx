6659399345bba97110840bfa84df6158a453b9b4
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-11-18 17:43:14 +0530

    See All Students Completed

--- admin/src/pages/SuperAdmin/Students/AllStudent.jsx
+++ admin/src/pages/SuperAdmin/Students/AllStudent.jsx
@@ -1,71 +1,111 @@
 import React from 'react'
+import fakeData from "./MockData.json";
+import { useTable, usePagination } from 'react-table'
 import './AllStudent.css'
 
 export default function AllStudent() {
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Enrollment No.",
+        accessor: "enrollmentNo",
+      },
+      {
+        Header: "Scholar No.",
+        accessor: "scholarNo",
+      },
+      {
+        Header: "Year",
+        accessor: "year",
+      },
+      {
+        Header: "Branch",
+        accessor: "branch",
+      },
+      {
+        Header: "Section",
+        accessor: "section",
+      },
+      {
+        Header: "Specialization",
+        accessor: "specialization",
+      },
+      {
+        Header: "Faculty",
+        accessor: "faculty",
+      },
+      {
+        Header: "Programme",
+        accessor: "programme",
+      }
+    ],
+    []
+  );
+
+  const initialState = {
+    pageSize: 20
+  }
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    prepareRow
+  } = useTable({ columns, data, initialState, enableEditing:true }, usePagination);
+
+  const { pageIndex } = state;
+
   return (
     <div className='allStudentMain'>
       <h2>All Students List</h2>
       <div className="allStudentTable">
-        <table>
+        <table className='adminStudentTable' {...getTableProps()}>
           <thead>
-            <tr>
-              <th>Name</th>
-              <th>Enrollment No.</th>
-              <th>Scholar No.</th>
-              <th>Faculty</th>
-              <th>Programme</th>
-              <th>Branch</th>
-              <th>Specialization</th>
-            </tr>
+            {headerGroups.map((headerGroup) => (
+              <tr className='adminStudentTableRow' {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th className='adminStudentTableHead' {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
           </thead>
-          <tbody>
-            <tr>
-              <td>Nehanshu Ajmera</td>
-              <td>EN20CS306038</td>
-              <td>2002537</td>
-              <td>Engineering</td>
-              <td>B-Tech</td>
-              <td>CSE</td>
-              <td>CSBS</td>
-            </tr>
-            <tr>
-              <td>Nehanshu Ajmera</td>
-              <td>EN20CS306038</td>
-              <td>2002537</td>
-              <td>Engineering</td>
-              <td>B-Tech</td>
-              <td>CSE</td>
-              <td>CSBS</td>
-            </tr>
-            <tr>
-              <td>Nehanshu Ajmera</td>
-              <td>EN20CS306038</td>
-              <td>2002537</td>
-              <td>Engineering</td>
-              <td>B-Tech</td>
-              <td>CSE</td>
-              <td>CSBS</td>
-            </tr>
-            <tr>
-              <td>Nehanshu Ajmera</td>
-              <td>EN20CS306038</td>
-              <td>2002537</td>
-              <td>Engineering</td>
-              <td>B-Tech</td>
-              <td>CSE</td>
-              <td>CSBS</td>
-            </tr>
-            <tr>
-              <td>Nehanshu Ajmera</td>
-              <td>EN20CS306038</td>
-              <td>2002537</td>
-              <td>Engineering</td>
-              <td>B-Tech</td>
-              <td>CSE</td>
-              <td>CSBS</td>
-            </tr>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr className='adminStudentTableRow' {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td className='adminStudentTableData' {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
+      </div>
+      <div className="tablePageButtons">
+        <button className='nAndpButtons' onClick={() => previousPage()} disabled={!canPreviousPage}> Previous </button>
+        <span className="pageNoDetails">
+          {' '}Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <button className='nAndpButtons' onClick={() => nextPage()} disabled={!canNextPage}> Next </button>
       </div>
     </div>
   )
