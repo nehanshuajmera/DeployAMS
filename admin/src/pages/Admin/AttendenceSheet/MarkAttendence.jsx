492a477d09697a9e9225766eea953ab657114a10
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-12-20 19:02:49 +0530

    pull and update

--- admin/src/pages/Admin/AttendenceSheet/MarkAttendence.jsx
+++ admin/src/pages/Admin/AttendenceSheet/MarkAttendence.jsx
@@ -3,6 +3,7 @@ import { useTable, usePagination, useSortBy, useGlobalFilter } from 'react-table
 import { useDispatch, useSelector } from 'react-redux';
 import GlobalFiltering from '../../../components/GlobalFiltering';
 import { fetchdetailasync } from '../../../redux-toolkit/slices/fetchdetailslice';
+import './MarkAttendance.css'
 
 export default function MarkAttendance() {
   const dispatch = useDispatch();
@@ -91,54 +92,66 @@ export default function MarkAttendance() {
   const { pageIndex, globalFilter } = state;
 
   return (
-    <div className='allStudentMain'>
-      <h2>Attendence Sheet</h2>
-      <GlobalFiltering filter={globalFilter} setFilter={setGlobalFilter} />
-      <div className="allStudentTable">
-        <table className='adminStudentTable' {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr className='adminStudentTableRow' {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th className='adminStudentTableHead' {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    {column.render("Header")}
-                    <span>
-                      {column.isSorted ? (column.isSortedDesc ? ' ⬇' : ' ⬆') : ' ↕'}
-                    </span>
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {page.map((row) => {
-              prepareRow(row);
-              console.log(row)
-              return (
-                <tr className='adminStudentTableRow' {...row.getRowProps()} onClick={() => gotoUpdate(row)}>
-                  {row.cells.map((cell) => (
-                    <td className='adminStudentTableData' {...cell.getCellProps()}>
-                      {cell.render("Cell")}
-                    </td>
+    <div className='markAttendanceMain'>
+      <div className='attendenceFormat'>
+        <h2>Attendence Sheet</h2>
+        <GlobalFiltering filter={globalFilter} setFilter={setGlobalFilter} />
+        <div className="studentAttendanceTable">
+          <table className='studentTable' {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr className='studentTableRow' {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th className='studentTableHead' {...column.getHeaderProps(column.getSortByToggleProps())}>
+                      {column.render("Header")}
+                      <span>
+                        {column.isSorted ? (column.isSortedDesc ? ' ⬇' : ' ⬆') : ' ↕'}
+                      </span>
+                    </th>
                   ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      {page.length ?
-        <div className="tablePageButtons">
-          <button className='nAndpButtons' onClick={() => previousPage()} disabled={!canPreviousPage}> Previous </button>
-          <span className="pageNoDetails">
-            {' '}Page{' '}
-            <strong>
-              {pageIndex + 1} of {pageOptions.length}
-            </strong>
-          </span>
-          <button className='nAndpButtons' onClick={() => nextPage()} disabled={!canNextPage}> Next </button>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {page.map((row) => {
+                prepareRow(row);
+                console.log(row)
+                return (
+                  <tr className='studentTableRow' {...row.getRowProps()} onClick={() => gotoUpdate(row)}>
+                    {row.cells.map((cell) => (
+                      <td className='studentTableData' {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-        : <h2 className="noData">No Data</h2>}
+        {page.length ?
+          <div className="tablePageButtons">
+            <button className='nAndpButtons' onClick={() => previousPage()} disabled={!canPreviousPage}> Previous </button>
+            <span className="pageNoDetails">
+              {' '}Page{' '}
+              <strong>
+                {pageIndex + 1} of {pageOptions.length}
+              </strong>
+            </span>
+            <button className='nAndpButtons' onClick={() => nextPage()} disabled={!canNextPage}> Next </button>
+          </div>
+          : <h2 className="noData">No Data</h2>}
+      </div>
+
+      <div className="askForPermission">
+        <div className="askForPermissionText">
+          <h2>Ask For Permission</h2>
+          <p>Please grant me permission to retake the attendence for Class Name of date</p>
+        </div>
+        <div className="askForPermissionBtn">
+          <button>Ask</button>
+        </div>
+      </div>
     </div>
   )
 }
