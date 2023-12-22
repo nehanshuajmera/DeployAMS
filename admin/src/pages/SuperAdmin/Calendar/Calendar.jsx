bdb3d7caa8e85bf11061fcbda55bbceedf03cf26
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-12-22 22:41:56 +0530

    Academic Calendar

--- admin/src/pages/SuperAdmin/Calendar/Calendar.jsx
+++ admin/src/pages/SuperAdmin/Calendar/Calendar.jsx
@@ -1,63 +1,403 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createAcademicAsync } from '../../../redux-toolkit/slices/academicCalenderslice'
+import './Calendar.css'
 
 const semDate = {
-  startDate : '',
-  endDate : '',
+  startDate: '',
+  endDate: '',
 }
 
 export default function Calendar() {
   const [dates, setDates] = useState(semDate)
   const dispatch = useDispatch()
 
-  const changeHandler = (e)=>{
-    setDates(prev=>{
-      return{
+  const changeHandler = (e) => {
+    setDates(prev => {
+      return {
         ...prev,
-        [e.target.name]:e.target.value,
+        [e.target.name]: e.target.value,
       }
     })
     console.log(dates)
   }
 
-  const HandleClick = ()=>{
-    
-      ;(async()=>{
-        try{  
-          await dispatch(createAcademicAsync({...dates}));
-  
-        }catch(error){
-            console.log(error);
-        }
-      })()
-   
+  const HandleClick = () => {
+
+    ; (async () => {
+      try {
+        await dispatch(createAcademicAsync({ ...dates }));
+
+      } catch (error) {
+        console.log(error);
+      }
+    })()
+
   }
 
   return (
-    <div className='flex justify-center align items-center w-full h-screen bg-dimWhite'>
+    <div className='flex justify-center w-full flex-col bg-dimWhite'>
       {/* Semister start-end Form  */}
-      <div className="addDateSide flex flex-col p-1 m-2 w-1/3 justify-center items-center">
-        <h3 className='m-1 p-1 text-center font-bold text-lg'>Academic Calendar</h3>
-        <div className="startDate">
-          <h4 className='font-semibold'>Enter Start Date</h4>
-          <div >
-            <label className='p-1 m-1' htmlFor="startDate">Start of Semester:</label>
-            <input className='p-1 m-1' type="date" id="startDate" name="startDate" value={dates.startDate} onChange={(e)=>changeHandler(e)} />
+      <div className="addDateSide flex p-1 justify-center items-center">
+        <div className='w-1/3 m-10 p-4 flex flex-col items-center justify-center'>
+          <h3 className='m-1 p-1 text-center font-bold text-lg'>Academic Calendar</h3>
+          <div className='flex items-center justify-center m-2'>
+            <div className='p-2 m-1 bg-black text-red-500 rounded'>Start Date: 22/01/2024</div>
+            <div className='p-2 m-1 bg-black text-red-500 rounded'>End Date: 22/04/2024</div>
           </div>
-        </div>
-        <div className="endDate">
-          <h4 className='font-semibold'>Enter End Date</h4>
-          <div >
-            <label className='p-1 m-1' htmlFor="endDate">End of Semester:</label>
-            <input className='p-1 m-1' type="date" id="endDate" name="endDate" value={dates.endDate} onChange={(e)=>changeHandler(e)} />
+          <div className="startDate">
+            <h4 className='font-semibold'>Enter Start Date</h4>
+            <div >
+              <label className='p-1 m-1' htmlFor="startDate">Start of Semester:</label>
+              <input className='p-1 m-1' type="date" id="startDate" name="startDate" value={dates.startDate} onChange={(e) => changeHandler(e)} />
+            </div>
           </div>
+          <div className="endDate">
+            <h4 className='font-semibold'>Enter End Date</h4>
+            <div >
+              <label className='p-1 m-1' htmlFor="endDate">End of Semester:</label>
+              <input className='p-1 m-1' type="date" id="endDate" name="endDate" value={dates.endDate} onChange={(e) => changeHandler(e)} />
+            </div>
+          </div>
+          <div className='button1 cursor-pointer mt-4 w-fit self-center' onClick={() => HandleClick()}>Submit</div>
         </div>
-        <div className='button1 cursor-pointer mt-4' onClick={()=>HandleClick()}>Submit</div>
+        <div>
+          <h3 className='m-1 p-1 text-center font-bold text-lg'>Add Holiday</h3>
+          <div className="holidayDate">
+            <div >
+              <label className='p-1 m-1' htmlFor="startDate">Holiday Date:</label>
+              <input className='p-1 m-1' type="date" id="startDate" name="startDate" value={dates.startDate} onChange={(e) => changeHandler(e)} />
+            </div>
+            <div>
+              <label className='p-1 m-1' for="holiday">Holiday name:</label>
+              <input className='p-1 m-1 border-2 border-gray-600 rounded' type="text" id="holiday" name="holiday"></input>
+            </div>
+          </div>
+          <div className='button1 cursor-pointer mt-4 w-fit self-center' onClick={() => HandleClick()}>Submit</div>
+        </div>
       </div>
-      {/* <div className="addHolidayInCalender w-1/2 border-2 border-red-600">
-        hello
-      </div> */}
+      <div className="addHolidayInCalender">
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+        <div className="dateRelatedData rounded">
+          <h4>Date</h4>
+          <h4>Day</h4>
+          <h4>Name Of Holiday</h4>
+        </div>
+      </div>
     </div>
   )
 }
