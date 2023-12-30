6026188350117f98b2b3541317cb547a345393d8
sharma39vishal <sharma39vishal@gmail.com>
2023-12-30 16:30:33 +0530

    AWS file backup

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const cron = require('node-cron');
-const updateTodayAttendance=require("./Controller/UpdateTodayAttendance");
+const updateTodayAttendance = require("./Controller/UpdateTodayAttendance");
+const backupDatabase = require("./Controller/DBbackup.js")
 const fileUpload = require('express-fileupload');
+const listFilesInBucket = require("./Controller/SeeAllAWSFiles.js");
+const exportToS3 = require("./Controller/SaveFiletoAWS.js");
 
 dotenv.config();
 app.use(express.json());
 app.use(fileUpload());
+
 
 app.use(cookieParser());
 
@@ -24,44 +28,53 @@ app.use(cors({
 })
 );
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 // connect to mongoDB
 
-
-
+// connect to mongoDB
 mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MDB_CONNECT) 
-.then(()=>{console.log('Mongodb connected'); });
+mongoose.connect(process.env.MDB_CONNECT)
+  .then(() => {
+    console.log('Mongodb connected');
 
-// set up routes
-app.use("/api/calander", require("./Routes/calanderRouter.js"));
-app.use("/api/teacher", require("./Routes/teacherRouter.js"));
-app.use("/api/authentic", require("./Routes/checkauthRouter.js"));
-app.use("/api/student", require("./Routes/studentRouter.js"));
-app.use("/api/admin", require("./Routes/adminRouter.js"));
-app.use("/api/logs", require("./Routes/logRouter.js"));
-app.use("/api/schedule",require("./Routes/scheduleRouter"));
-app.use("/api/updateattendance",require("./Routes/updateattendanceRouter"));
-app.use("/api/alert",require("./Routes/alertRouter"));
-app.use("/api/xlsx",require("./Routes/xlsxRouter"));
+    // set up routes
+    app.use("/api/calander", require("./Routes/calanderRouter.js"));
+    app.use("/api/teacher", require("./Routes/teacherRouter.js"));
+    app.use("/api/authentic", require("./Routes/checkauthRouter.js"));
+    app.use("/api/student", require("./Routes/studentRouter.js"));
+    app.use("/api/admin", require("./Routes/adminRouter.js"));
+    app.use("/api/logs", require("./Routes/logRouter.js"));
+    app.use("/api/schedule", require("./Routes/scheduleRouter"));
+    app.use("/api/updateattendance", require("./Routes/updateattendanceRouter"));
+    app.use("/api/alert", require("./Routes/alertRouter"));
+    app.use("/api/xlsx", require("./Routes/xlsxRouter"));
+    
+    
+    // Schedule the cron jobs
+    cron.schedule('31 5 * * *', async () => {
+      console.log('Running teacher rating update job...');
+      try {
+        const result = await updateTodayAttendance();
+        await backupDatabase();
+        await exportToS3();
+        console.log(result);
+      } catch (error) {
+        console.error('Error updating attendance:', error);
+      }
+    });
 
-
-// Schedule the cron job to run at 5:31 am every day
-cron.schedule('31 5 * * *', async () => {
-  console.log('Running teacher rating update job...');
-  try {
-    const result = await updateTodayAttendance();
-    console.log(result);
-  } catch (error) {
-    console.error('Error updating attendance:', error);
-  }
-});
-
-
-app.listen(PORT, err => {
-  if (err) throw err;
-  console.log(`Server started on port: ${PORT}`);
-});
-
-
-updateTodayAttendance()
+    // backupDatabase();
+    // listFilesInBucket();
+    
+    app.listen(PORT, err => {
+      if (err) throw err;
+      console.log(`Server started on port: ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
