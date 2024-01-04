24215f3e5b1ea6f26f14ce73136b2f85b9251c09
sharma39vishal <sharma39vishal@gmail.com>
2024-01-04 11:13:08 +0530

    1 min 300req limit

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.use((err, req, res, next) => {
 
 // Use rate limiting middleware
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10000, // limit each IP to 100 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 300, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes"
 });
 
