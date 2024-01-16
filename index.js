6f432c3bc155eaea3e992093e80da4ee68b3172f
sharma39vishal <sharma39vishal@gmail.com>
2024-01-16 23:42:30 +0530

    add gmail

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "http://localhost:5174"
+    "https://medicaps-ams-student.netlify.app"
   ],
   credentials: true,
 })
